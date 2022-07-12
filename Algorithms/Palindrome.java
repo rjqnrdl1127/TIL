165e95287959363051f90e1a7276126469bd6222
rjqnrdl1127 <56789096+rjqnrdl1127@users.noreply.github.com>
2022-07-13 01:05:34 +0900

    자료 추가

---
+++ Algorithms/Palindrome.java
@@ -0,0 +1,26 @@
+package Algorithms;
+
+import java.io.*;
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        int count = 0;
+        String s;
+        StringBuilder sb;
+
+        for (int i = n; i <= m; i++) {
+            s = String.valueOf(i);
+            sb = new StringBuilder(s);
+
+            if (s.equals(sb.reverse().toString())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
