18bb4cc4bd7a750845d966050ae0801909217992
seongmin <rjqnrdl1127@naver.com>
2022-07-17 12:49:25 +0900

    수정

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
-        int[] Rectangle = {n, n * 2};
-
 
     }
 }
